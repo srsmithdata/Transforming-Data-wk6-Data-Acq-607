3ec1f5d5060ccee345e55e44bee1456910d17879
srsmithdata <rsmith.data@gmail.com>
2016-03-06 23:26:52 -0500

    Initial upload

---
+++ DataTrans1.R
@@ -0,0 +1,40 @@
+# Data wrangling with R and R Studio notes
+# Script document
+#
+# from the video (not needed for assignment
+## require(devtools)
+## devtools::install_github("rstudio/EDAWR")
+##
+##
+
+# ---- Test 1 ----
+
+# install.packages('dplyr')
+require('dplyr')
+# install.packages('tidyr')
+require('tidyr')
+# install.packages('ggplot2')
+require('ggplot2')
+# install.packages{"devtools")
+require(devtools)
+# install.packages{"rmarkdown")
+require(rmarkdown)
+# install.packages{"knitr")
+require(knitr)
+# install.packages("RMySQL")
+require(RMySQL)
+# install.packages{"xtable")
+require(xtable)
+# install.packages{"stringr")
+require(stringr)
+# install.packages{"XML")
+require(XML)
+# install.packages{"RCurl")
+require(RCurl)
+
+```
+
+install.packages("
+install.packages("tidyr")
+require("tidyr")
+
